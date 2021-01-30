664064d38d6fd3d1753c6f949df25aaba76594f1
emrdagkusu <emrdagkusu@E.D>
2021-01-30 18:48:05 +0300

    semester crud operations added

--- backend/apps/task/models.py
+++ backend/apps/task/models.py
@@ -1,47 +1,8 @@
-from typing import Optional
+from typing import Optional, List, Dict
 import uuid
 from pydantic import BaseModel, Field, EmailStr
 from datetime import datetime
-from typing import List
 
-
-class UserModel(BaseModel):
-    id: str = Field(defaul_factory=uuid.uuid4, alias="_id")
-    email: EmailStr = Field(unique=True)
-    password: str = Field(...)
-
-
-
-
-
-    
-    # semesters: List[SemesterModel]
-    
-    class Config:
-        allow_population_by_field_name = True
-        schema_extra = {
-            "example": {
-                "id": "00010203-0405-0607-0809-0a0b0c0d0e0u",
-
-                "email": "hello@agu.edu.tr",
-
-                "password": "hello1234",
-            }
-        }
-
-
-class UpdateUserModel(BaseModel):
-    email: Optional[EmailStr]
-    password: Optional[str]
-
-    class Config:
-        allow_population_by_field_name = True
-        schema_extra = {
-            "example": {
-                "email": "hello@agu.edu.tr",
-                "password": "hello1234",
-            }
-        }
 
 class LessonModel(BaseModel):
     name: str = Field(...)
@@ -52,29 +13,24 @@ class LessonModel(BaseModel):
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
-                "name": "EE201",
+                "name": "EE203",
                 "instructor": "Ali Veli",
-                "sections": [
-                    ["W", "7"],
-                    ["W", "8"]
-                ]
+                "sections": [["W", "7"], ["W", "8"]],
             }
         }
 
 
 class UpdateLessonModel(BaseModel):
+    name: Optional[str]
     instructor: Optional[str]
     sections: Optional[List[List[str]]]
-
 
     class Config:
         schema_extra = {
             "example": {
+                "name": "EE203",
                 "instructor": "Ali Veli",
-                "sections": [
-                    ["W", "7"],
-                    ["W", "8"]
-                ]
+                "sections": [["W", "7"], ["W", "8"]],
             }
         }
 
@@ -82,39 +38,32 @@ class UpdateLessonModel(BaseModel):
 class SemesterModel(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     name: str = Field(...)
-    owner: str = Field(...)
     startDate: datetime = Field(...)
-    endDate: datetime = Field(...) 
-    startHour: str = Field(...) 
+    endDate: datetime = Field(...)
+    startHour: str = Field(...)
     dLesson: int = 0
     dBreak: int = 0
     slotCount: int = 0
-    # lessons: Dict[str, LessonModel] = Field(...)
-    lessons: List[LessonModel] = Field(...)
+    lessons: Dict[str, LessonModel] = {}
 
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
-                "id": "00010203-0405-0607-0809-0a0b0c0d0e0z",
                 "name": "2020-21 Spring",
-                "owner": "user_nameORuser_id",
                 "startDate": "2016-02-18T16:00:00Z",
                 "endDate": "2016-06-18T16:00:00Z",
                 "startHour": "8.10",
                 "dLesson": 50,
                 "dBreak": 10,
                 "slotCount": 12,
-                "lessons": [
-                    {
+                "lessons": {
+                    "EE203": {
                         "name": "EE203",
                         "instructor": "Ali Veli",
-                        "sections": [
-                            ["W", "7"],
-                            ["W", "8"]
-                        ]
+                        "sections": [["W", "7"], ["W", "8"]],
                     },
-                    {
+                    "EE213": {
                         "name": "EE213",
                         "instructor": "Ali Ahmet",
                         "sections": [
@@ -122,22 +71,23 @@ class SemesterModel(BaseModel):
                             ["M", "5"],
                             ["T", "2"],
                             ["T", "3"],
-                            ["T", "4"]
-                        ]
+                            ["T", "4"],
+                        ],
                     },
-                ],
+                },
             }
         }
+
 
 class UpdateSemesterModel(BaseModel):
     name: Optional[str]
     startDate: Optional[datetime]
     endDate: Optional[datetime]
-    startHour: Optional[str] 
+    startHour: Optional[str]
     dLesson: Optional[int]
     dBreak: Optional[int]
     slotCount: Optional[int]
-    lessons: Optional[List[str]] # Instead of list of strs there will be list of lessons
+    lessons: Optional[Dict[str, LessonModel]]
 
     class Config:
         allow_population_by_field_name = True
@@ -150,35 +100,43 @@ class UpdateSemesterModel(BaseModel):
                 "dLesson": 50,
                 "dBreak": 10,
                 "slotCount": 12,
-                "lessons": ["OHS", "TURK101", "HIST201"],
+                "lessons": {
+                    "EE203": {
+                        "name": "EE203",
+                        "instructor": "Ali Veli",
+                        "sections": [["W", "7"], ["W", "8"]],
+                    },
+                },
             }
         }
 
 
-class TaskModel(BaseModel):
+class UserModel(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    name: str = Field(...)
-    completed: bool = False
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+    semesters: List[SemesterModel] = []
 
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
-                "id": "00010203-0405-0607-0809-0a0b0c0d0e0f",
-                "name": "My important task",
-                "completed": True,
+                "email": "hello@agu.edu.tr",
+                "password": "hello1234",
             }
         }
 
 
-class UpdateTaskModel(BaseModel):
-    name: Optional[str]
-    completed: Optional[bool]
+class UpdateUserModel(BaseModel):
+    email: Optional[EmailStr]
+    password: Optional[str]
+    semesters: Optional[List[SemesterModel]]
 
     class Config:
+        allow_population_by_field_name = True
         schema_extra = {
             "example": {
-                "name": "My important task",
-                "completed": True,
+                "email": "hello@agu.edu.tr",
+                "password": "hello1234",
             }
         }
--- backend/apps/task/user_routers.py
+++ backend/apps/task/user_routers.py
@@ -45,7 +45,9 @@ async def update_user(uid: str, request: Request, user: UpdateUserModel = Body(.
 
         if update_result.modified_count == 1:
             if (
-                updated_user := await request.app.mongodb["users"].find_one({"_id": uid})
+                updated_user := await request.app.mongodb["users"].find_one(
+                    {"_id": uid}
+                )
             ) is not None:
                 return updated_user
 
@@ -69,65 +71,104 @@ async def delete_user(uid: str, request: Request):
 
 # Semester
 
-# We post semester into semesters collection in the db
-# /{id}/semesters doesn't necessary
-@router.post("/{uid}/semesters", response_description="Add new semester")
-async def create_semester(request: Request, semester: SemesterModel = Body(...)):
-    semester = jsonable_encoder(semester)
-    new_semester = await request.app.mongodb["semesters"].insert_one(semester)
-    created_semester = await request.app.mongodb["semesters"].find_one(
-        {"_id": new_semester.inserted_id}
-    )
 
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_semester)
+@router.post("/{uid}/semesters", response_description="Add new semester")
+async def create_semester(
+    uid: str,
+    request: Request,
+    semester: SemesterModel = Body(...),
+):
+    semester = jsonable_encoder(semester)
+
+    if (user := await request.app.mongodb["users"].find_one({"_id": uid})) is not None:
+        update_result = await request.app.mongodb["users"].update_one(
+            {"_id": uid}, {"$push": {"semesters": semester}}
+        )
+
+        if update_result.modified_count == 1:
+            if (
+                user := await request.app.mongodb["users"].find_one({"_id": uid})
+            ) is not None:
+                return user["semesters"]
+
+    if (user := await request.app.mongodb["users"].find_one({"_id": uid})) is not None:
+        return user["semesters"]
+
+    raise HTTPException(status_code=404, detail=f"User {uid} not found")
+
 
 @router.get("/{uid}/semesters", response_description="List all semesters")
 async def list_semesters(uid: str, request: Request):
-    semesters = []
-    for doc in await request.app.mongodb["semesters"].find({"owner": uid}).to_list(length=100):
-        semesters.append(doc)
-    if (len(semesters) != 0):
-        return semesters
+    if (user := await request.app.mongodb["users"].find_one({"_id": uid})) is not None:
+        return user["semesters"]
 
     raise HTTPException(status_code=404, detail=f"Semesters of user {uid} not found")
 
 
 @router.get("/{uid}/semesters/{sid}", response_description="Get a single semester")
 async def show_semester(uid: str, sid: str, request: Request):
-    if (semester := await request.app.mongodb["semesters"].find_one({"_id": sid, "owner": uid})) is not None:
-        return semester
+    if (user := await request.app.mongodb["users"].find_one({"_id": uid})) is not None:
+        for semester in user["semesters"]:
+            if semester["_id"] == sid:
+                return semester
 
     raise HTTPException(status_code=404, detail=f"Semester {sid} not found")
 
 
 @router.put("/{uid}/semesters/{sid}", response_description="Update a semester")
-async def update_semester(uid: str, sid: str, request: Request, semester: UpdateSemesterModel = Body(...)):
+async def update_semester(
+    uid: str, sid: str, request: Request, semester: UpdateSemesterModel = Body(...)
+):
     semester = {k: v for k, v in semester.dict().items() if v is not None}
 
     if len(semester) >= 1:
-        update_result = await request.app.mongodb["semesters"].update_one(
-            {"_id": sid, "owner": id}, {"$set": semester}
+        update_result = await request.app.mongodb["users"].update_many(
+            {"_id": uid, "semesters._id": sid},
+            {
+                "$set": {
+                    "semesters.$.name": semester["name"],
+                    "semesters.$.startDate": semester["startDate"],
+                    "semesters.$.endDate": semester["endDate"],
+                    "semesters.$.startHour": semester["startHour"],
+                    "semesters.$.dLesson": semester["dLesson"],
+                    "semesters.$.dBreak": semester["dBreak"],
+                    "semesters.$.slotCount": semester["slotCount"],
+                }
+            },
         )
 
-        if update_result.modified_count == 1:
-            if (
-                updated_task := await request.app.mongodb["semesters"].find_one({"_id": sid, "owner": uid})
-            ) is not None:
-                return updated_task
+        if (
+            updated_user := await request.app.mongodb["users"].find_one({"_id": uid})
+        ) is not None:
+            return updated_user
 
     if (
-        existing_semester := await request.app.mongodb["semesters"].find_one({"_id": sid, "owner": uid})
+        updated_user := await request.app.mongodb["users"].find_one({"_id": uid})
     ) is not None:
-        return existing_semester
+        return updated_user
 
     raise HTTPException(status_code=404, detail=f"Semester {sid} not found")
 
 
 @router.delete("/{uid}/semesters/{sid}", response_description="Delete semester")
-async def delete_semester(sid: str, request: Request):
-    delete_result = await request.app.mongodb["semesters"].delete_one({"_id": sid})
+async def delete_semester(uid: str, sid: str, request: Request):
 
-    if delete_result.deleted_count == 1:
-        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+    if (user := await request.app.mongodb["users"].find_one({"_id": uid})) is not None:
+        update_result = await request.app.mongodb["users"].update_one(
+            {"_id": uid}, {"$pull": {"semesters": {"_id": sid}}}
+        )
 
-    raise HTTPException(status_code=404, detail=f"Semester {sid} not found")
+        if update_result.modified_count == 1:
+            if (
+                updated_user := await request.app.mongodb["users"].find_one(
+                    {"_id": uid}
+                )
+            ) is not None:
+                return updated_user
+
+    if (
+        existing_user := await request.app.mongodb["users"].find_one({"_id": uid})
+    ) is not None:
+        return existing_user
+
+    raise HTTPException(status_code=404, detail=f"User {uid} not found")
