974fe2da4192c79634e87bcbf325851887cfa8b9
emrdagkusu <emrdagkusu@E.D>
2021-02-13 22:52:35 +0300

    delete warning added

--- frontend/src/components/Semester.js
+++ frontend/src/components/Semester.js
@@ -1,10 +1,8 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Semester = ({ semester, onDeleteSemester, onCurrentSemester, currentSemester }) => {
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
-        "July", "Aug", "Sep", "Oct", "Nov", "Dec"
-    ];
-
+    const [semesterDeleteModal, setSemesterDeleteModal] = useState(false);
     const resStartDate = String(semester.startDate).split(" ");
     const startDate = resStartDate[1] + " " + resStartDate[2] + ", " + resStartDate[3]
 
@@ -34,13 +32,68 @@ const Semester = ({ semester, onDeleteSemester, onCurrentSemester, currentSemest
                     </span>
                     <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
                         <span aria-hidden className="absolute inset-0 bg-red-500 opacity-90 rounded-full"></span>
-                        <button onClick={onDeleteSemester} className="relative text-xs font-bold">Delete</button>
+                        <button onClick={() => setSemesterDeleteModal(true)} type="button"
+                            style={{ transition: "all .15s ease" }}
+                            className="relative text-xs font-bold">Delete</button>
                     </span>
                 </div>
             </td>
             <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
-                <Link to={{ pathname: `/semesters/${semester.id}`, newSemester: semester }} className="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Details</Link>
+                <Link to={{ pathname: `/semesters/${semester.id}` }} className="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Details</Link>
             </td>
+
+            {semesterDeleteModal ? (
+                <div>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                                    <h3 className="pt-4 text-2xl text-center">Delete Semester: {semester.name}</h3>
+
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setSemesterDeleteModal(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                    </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <p className="flex justify-center text-base m-8">You will lose everything belongs to this semester irreversibly!</p>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
+                                    <button
+                                        className="background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                                        type="button"
+                                        style={{ transition: "all .15s ease" }}
+                                        onClick={() => setSemesterDeleteModal(false)}
+                                    >
+                                        Close
+                  </button>
+                                    <button
+                                        className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                                        type="button"
+                                        style={{ transition: "all .15s ease" }}
+                                        onClick={() => {
+                                            setSemesterDeleteModal(false)
+                                            onDeleteSemester()
+                                        }
+                                        }
+                                    >
+                                        Delete Anyway
+                  </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </div>
+            ) : null}
         </tr>
     )
 }
