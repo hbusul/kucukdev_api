8f79acb25dbb7e4d9bd510f614144551f999d5e1
yusufuysal <yusuf.uysal@agu.edu.tr>
2022-02-06 01:06:22 +0300

    add semester routers tests

--- backend/app/tests/test_semester_routers.py
+++ backend/app/tests/test_semester_routers.py
@@ -45,6 +45,28 @@ def test_create_semester():
     test_user.semester_id = semester.json()["_id"]
 
 
+def test_create_semester_with_another_user_id():
+    """ "Test creating semester with a user id other than auth user"""
+
+    user_id = "non_existent_user_id"
+    semester = client.post(
+        f"/users/{user_id}/semesters",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+        json={
+            "name": "Test Semester",
+            "startDate": "2022-02-18T00:00:00Z",
+            "endDate": "2022-06-18T00:00:00Z",
+            "startHour": "8.20",
+            "dLesson": 40,
+            "dBreak": 20,
+            "slotCount": 12,
+        },
+    )
+
+    assert semester.status_code == 403
+    assert semester.json()["message"] == "No right to access"
+
+
 def test_get_semester():
     """Test getting semester"""
 
@@ -64,3 +86,232 @@ def test_get_semester():
         "dBreak": 20,
         "slotCount": 12,
     }
+
+
+def test_get_semester_with_another_user_id():
+    """ "Test getting semester with a user id other than auth user"""
+
+    user_id = "non_existent_user_id"
+    semester = client.get(
+        f"/users/{user_id}/semesters/{test_user.semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semester.status_code == 403
+    assert semester.json()["message"] == "No right to access"
+
+
+def test_get_semester_with_invalid_semester_id():
+    """ "Test getting semester with an invalid semester id"""
+
+    semester_id = "non_existent_semester_id"
+    semester = client.get(
+        f"/users/{test_user.user_id}/semesters/{semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semester.status_code == 404
+    assert semester.json()["message"] == "Semester not found"
+
+
+def test_update_semester():
+    """Test updating semester"""
+
+    semester = client.put(
+        f"/users/{test_user.user_id}/semesters/{test_user.semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+        json={
+            "name": "Test Semester 2",
+            "startDate": "2022-02-18T00:00:00+00:00",
+            "endDate": "2022-06-18T00:00:00+00:00",
+            "startHour": "8.20",
+            "dLesson": 40,
+            "dBreak": 20,
+            "slotCount": 12,
+        },
+    )
+
+    assert semester.status_code == 200
+    assert semester.json()["message"] == "Semester updated"
+
+
+def test_update_semester_with_another_user_id():
+    """ "Test updating semester with a user id other than auth user"""
+
+    user_id = "non_existent_user_id"
+    semester = client.put(
+        f"/users/{user_id}/semesters/{test_user.semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+        json={
+            "name": "Test Semester 2",
+            "startDate": "2022-02-18T00:00:00+00:00",
+            "endDate": "2022-06-18T00:00:00+00:00",
+            "startHour": "8.20",
+            "dLesson": 40,
+            "dBreak": 20,
+            "slotCount": 12,
+        },
+    )
+
+    assert semester.status_code == 403
+    assert semester.json()["message"] == "No right to access"
+
+
+def test_update_semester_with_invalid_semester_id():
+    """ "Test updating semester with an invalid semester id"""
+
+    semester_id = "non_existent_semester_id"
+    semester = client.put(
+        f"/users/{test_user.user_id}/semesters/{semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+        json={
+            "name": "Test Semester 2",
+            "startDate": "2022-02-18T00:00:00+00:00",
+            "endDate": "2022-06-18T00:00:00+00:00",
+            "startHour": "8.20",
+            "dLesson": 40,
+            "dBreak": 20,
+            "slotCount": 12,
+        },
+    )
+
+
+def test_list_semesters():
+    """Test listing semesters"""
+
+    semesters = client.get(
+        f"/users/{test_user.user_id}/semesters",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semesters.status_code == 200
+    # semesters.json() only includes our last semester that created and updated
+    assert semesters.json() == [
+        {
+            "_id": test_user.semester_id,
+            "name": "Test Semester 2",
+            "startDate": "2022-02-18T00:00:00+00:00",
+            "endDate": "2022-06-18T00:00:00+00:00",
+            "startHour": "8.20",
+            "dLesson": 40,
+            "dBreak": 20,
+            "slotCount": 12,
+        }
+    ]
+
+
+def test_semesters_with_another_user_id():
+    """ "Test listing semesters with a user id other than auth user"""
+
+    user_id = "non_existent_user_id"
+    semesters = client.get(
+        f"/users/{user_id}/semesters/{test_user.semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semesters.status_code == 403
+    assert semesters.json()["message"] == "No right to access"
+
+
+def test_create_second_semester():
+    """ "Test creating second semester"""
+
+    semester = client.post(
+        f"/users/{test_user.user_id}/semesters",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+        json={
+            "name": "Test Semester 3",
+            "startDate": "2023-02-18T00:00:00+00:00",
+            "endDate": "2023-06-18T00:00:00+00:00",
+            "startHour": "8.20",
+            "dLesson": 40,
+            "dBreak": 20,
+            "slotCount": 12,
+        },
+    )
+
+    assert semester.status_code == 201
+    assert semester.json()["message"] == "Semester created"
+
+
+def test_list_semesters_with_two_semesters():
+    """ "Test listing semesters with two semesters"""
+
+    semesters = client.get(
+        f"/users/{test_user.user_id}/semesters",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semesters.status_code == 200
+    assert semesters.json() == [
+        {
+            "_id": semesters.json()[0]["_id"],
+            "name": "Test Semester 2",
+            "startDate": "2022-02-18T00:00:00+00:00",
+            "endDate": "2022-06-18T00:00:00+00:00",
+            "startHour": "8.20",
+            "dLesson": 40,
+            "dBreak": 20,
+            "slotCount": 12,
+        },
+        {
+            "_id": semesters.json()[1]["_id"],
+            "name": "Test Semester 3",
+            "startDate": "2023-02-18T00:00:00+00:00",
+            "endDate": "2023-06-18T00:00:00+00:00",
+            "startHour": "8.20",
+            "dLesson": 40,
+            "dBreak": 20,
+            "slotCount": 12,
+        },
+    ]
+
+
+def test_delete_semester():
+    """Test deleting semester"""
+
+    semester = client.delete(
+        f"/users/{test_user.user_id}/semesters/{test_user.semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semester.status_code == 200
+    assert semester.json()["message"] == "Semester deleted"
+
+
+def test_delete_same_semester():
+    """ "Test deleting same semester"""
+
+    semester = client.delete(
+        f"/users/{test_user.user_id}/semesters/{test_user.semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semester.status_code == 404
+    assert semester.json()["message"] == "Semester not found"
+
+
+def test_delete_semester_with_another_user_id():
+    """ "Test deleting semester with a user id other than auth user"""
+
+    user_id = "non_existent_user_id"
+    semester = client.delete(
+        f"/users/{user_id}/semesters/{test_user.semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semester.status_code == 403
+    assert semester.json()["message"] == "No right to access"
+
+
+def test_delete_semester_with_invalid_semester_id():
+    """ "Test deleting semester with an invalid semester id"""
+
+    semester_id = "non_existent_semester_id"
+    semester = client.delete(
+        f"/users/{test_user.user_id}/semesters/{semester_id}",
+        headers={"Authorization": f"Bearer {test_user.token}"},
+    )
+
+    assert semester.status_code == 404
+    assert semester.json()["message"] == "Semester not found"
