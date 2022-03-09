3e925815dbcef37baaf6d0638c466354211ef85f
hbusul <h.burak.usul@gmail.com>
2022-03-09 16:20:39 +0100

    update init script for keycloak and fix env vars so it connects to database

--- init_keycloak.sh
+++ init_keycloak.sh
@@ -6,8 +6,5 @@ set -e
 [ "${KEYCLOAK_ADMIN_PASSWORD}" = "" ] && { echo "KEYCLOAK_ADMIN_PASSWORD is not specified"; exit 1; }
 
 ./"$HOME"/bin/kcadm.sh config credentials --server "${KEYCLOAK_URL}" --realm master --user "${KEYCLOAK_ADMIN}" --password "${KEYCLOAK_ADMIN_PASSWORD}" --client admin-cli
-./"$HOME"/bin/kcadm.sh create realms -f /tmp/realm-export.json
 
-
-echo "hello world"
-sleep inf
+./"$HOME"/bin/kcadm.sh get realms/kucukdev > /dev/null || ./"$HOME"/bin/kcadm.sh create realms -f /tmp/realm-export.json
